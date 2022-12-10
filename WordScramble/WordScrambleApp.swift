54befe62d2cf87c485e3bd366e4f03df2d567008
thaddeus0605 <tdronski@gmail.com>
2022-12-10 17:14:53 -0600

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Thaddeus Dronski on 12/10/22.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
